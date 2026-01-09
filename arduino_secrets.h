406632924f9ff79694d19d61aa43b1971c2f5f26
조현우 <chohyunwoo23@gmail.com>
2026-01-10 03:42:51 +0900

    Update arduino_secrets.h

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,3 +1,4 @@
 #define SECRET_SSID "12345678"
 #define SECRET_PASS "12345678!@#"
-#define SECRET_IP "192.168.60.155" // Nest.js 서버의 로컬 IP 주소
+#define SECRET_IP "192.168.60.155"
+#define SECRET_PORT "3000"
