654dd98edbb37629359d2dc6e34723fb6cf2371c
조현우 <chohyunwoo23@gmail.com>
2026-01-10 04:03:25 +0900

    Update arduino_secrets.h

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -2,3 +2,16 @@
 #define SECRET_PASS ""
 #define SECRET_IP ""
 #define SECRET_PORT ""
+#define SECRET_ENDPOINT ""
+
+#define SECRET_DHT ""
+#define SECRET_GAS ""
+#define SECRET_PULSE ""
+
+const char NESTJS_ENDPOINT[] = "/receive-data"; 
+
+const int DHT_DATA_PIN = 2;
+const int DHT_TYPE = DHT11;
+
+const int GAS_SENSOR_PIN = A0;
+const int PULSE_SENSOR_PIN = A1;
